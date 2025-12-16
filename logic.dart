173e9a65bbe98bd00b57c0c4b07e07ec5a080722
treeplate <51892395+treeplate@users.noreply.github.com>
2025-12-16 00:37:59 +0000

    changes (?)

--- logic.dart
+++ logic.dart
@@ -1,16 +1,24 @@
 import 'package:quiver/core.dart';
 
+List<String> colors = ["\u001b[31m", "\u001b[32m", "\u001b[33m", "\u001b[34m", "\u001b[35m"];
+
 class Vec2 {
   Vec2(this.dx, this.dy);
   Vec2.north(): dx = 0, dy = -1;
   Vec2.south(): dx = 0, dy = 1;
   Vec2.east(): dx = 1, dy = 0;
   Vec2.west(): dx = -1, dy = 0;
+  factory Vec2.ihat() => Vec2.east();
+  factory Vec2.jhat() => Vec2.south();
   final int dx;
   final int dy;
 
   Vec2 operator -() {
     return Vec2(-dx, -dy);
+  }
+
+  Vec2 operator *(int scalar) {
+    return Vec2(dx*scalar, dy*scalar);
   }
 
   Vec2 operator +(Vec2 other) => Vec2(other.dx+dx, other.dy+dy);
@@ -23,7 +31,7 @@ class Vec2 {
     return Vec2(rotate(clockwise, dx, dy), rotate(!clockwise, dy, dx));
   }
 
-  String toString() => "$dx, $dy";
+  String toString() => "$dx: $dy";
 }
 
 int rotate(bool clockwise, int target, int helper) {
@@ -31,33 +39,165 @@ int rotate(bool clockwise, int target, int helper) {
   if(direction == 0) return 0; else return target+direction;
 } 
 
-class Ball {
-  Ball(this.position, this.velocity);
+abstract class Thing {}
+
+class Ball extends Thing {
+  Ball(this.position, this.velocity, [int oldcolor, this.generator = false, this.genl = 5]): color = oldcolor ?? nC {
+    if(oldcolor == null) {
+      nC = (nC + 1) % colors.length;
+    }
+  }
+  bool generator;
+  int genri = 0;
+  int genl;
   final Vec2 position;
   final Vec2 velocity;
-  String toString() => "Ball(position: Pos2($position), velocity: Vec2($velocity))";
+  final int color;
+  static int nC = 0;
+  String toString() => generator ? "$genri$genri" : colors[color] + "$genri$genri" + "\u001b[0m";//(velocity == Vec2.north() ? "A" : velocity == Vec2.west() ? "<" : velocity == Vec2.south() ? "v" : ">") + "\u001b[0m";
   Ball getUpdatedBall() {
-    return Ball(position + velocity, velocity);
+    return Ball(position + velocity, velocity, genri == genl-1 ? null : color % colors.length);
   }
+}
+
+class Wall extends Thing {
+  String toString() => "##";
 }
 
 class Gameboard {
-  
-  /// The current state of the model.
-  /// The integer means how many balls there are in that 
-  /// square.
-  /// -1 means wall.
-  List<List<int>> get grid => null; //TODO: grid
-  /// Ticks the game
-  void tick() {
-    //TODO: implement tick
-    // - move balls
-    //   - for each ball
-    //     - change position by velocity
-    // - handle collisions
-    //   - have a function that takes a list of balls and
-    //   - returns a list of balls that should have the 
-    //   - same positions but different velocities
+  Gameboard(this.grid, this.width);
+
+  factory Gameboard.parse(String filecont) {
+    List<String> data = filecont.split('\n');
+    int y = 0;
+    int width = 0;
+    List<List<Thing>> parsed = [];
+    for (String line in data.toList()) {
+      width = 0;
+      //print("'" + line + "'");
+      cols:
+      for (String char in line.split('')) {
+        switch (char) {
+          case " ":
+            parsed.add([]);
+            break;
+          case "#":
+            parsed.add([Wall()]);
+            break;
+          case "v":
+            parsed.add([Ball(Vec2(width, y), Vec2.south())]);
+            break;
+          case "s":
+            parsed.add([Ball(Vec2(width, y), Vec2.south(), null, true)]);
+            break;
+          case ">":
+            parsed.add([Ball(Vec2(width, y), Vec2.east())]);
+            break;
+          case "<":
+            parsed.add([Ball(Vec2(width, y), Vec2.west())]);
+            break;
+          case "a":
+            parsed.add([Ball(Vec2(width, y), Vec2.west(), null, true, 3)]);
+            break;
+          case "A":
+            parsed.add([Ball(Vec2(width, y), Vec2.north())]);
+            break;
+          case "n":
+            parsed.add([Ball(Vec2(width, y), Vec2.north(), null, true)]);
+            break;
+          default:
+            throw FormatException(
+                "Unexpected \"$char\"(${char.runes.first}) while parsing Gameboard");
+        }
+        width++;
+      }
+      y++;
+    }
+    //print("the W is $width");
+    return Gameboard(
+      parsed,
+      width,
+    );
   }
 
+  final List<List<Thing>> grid;
+  final int width;
+
+  String toString() {
+    String str = "";
+    for(int i = 0; i < grid.length; i++) {
+      if(i != 0 && i % width == 0) str += '\n';
+      if(grid[i].length == 0) { str += "  "; continue;}
+      if(grid[i].length == 1) str += "${grid[i].single}";//${grid[i].single}";
+      else if(grid[i].whereType<Ball>().toList().length == 1) str += "${colors[grid[i].whereType<Ball>().toList().single.color]}XX\u001b[0m";
+       else str += "XX"; 
+    }
+    return str;
+  }
+
+  /// Ticks the game
+  void tick() {
+    List<Ball> balls = grid
+    .where(
+      (List<Thing> things) => things.any(
+        (Thing thing) => thing is Ball
+      )
+    )
+    .expand(
+      (List<Thing> things) => things.whereType<Ball>()
+    )
+    .toList();
+
+    for(Ball ball in balls) {
+      Ball newBall = ball.getUpdatedBall();
+      if(!ball.generator) {
+        grid[ball.position.dx + (ball.position.dy * width)].remove(ball);
+      }
+      if(ball.generator) {
+        ball.genri = (ball.genri + 1) % ball.genl;
+      }
+      if(newBall.position.dy < 0) continue;
+      if(newBall.position.dx >= width) continue;
+      if(newBall.position.dx < 0) continue;
+      if(newBall.position.dy >= grid.length / width) continue;
+      if(ball.genri == 0) {
+        grid[newBall.position.dx + (newBall.position.dy * width)].add(newBall);
+      }
+    }
+
+    List<List<Thing>> collisions = grid.where((List<Thing> things) => things.length > 1).toList();
+
+    for(List<Thing> things in collisions) {
+      List<Thing> result = handleCollision(things).toList();
+      things..clear()..addAll(result);
+    }
+  }
 }
+
+Iterable<Thing> handleCollision(List<Thing> things) sync* {
+  if(things.length < 2) yield things[0];
+  else {
+  if(things.any((Thing thing) => thing is Wall || (thing as Ball).generator)) {
+    for(Thing thing in things) {
+      if(thing is Ball && !thing.generator) yield Ball(thing.position, -thing.velocity, thing.color); else yield thing;
+    }
+  } else {
+    if(things.length > 2) {
+      for(Thing thing in things) {
+        if(thing is Ball) 
+          yield Ball(thing.position, -thing.velocity, thing.color);
+      }
+    } else {
+      if((things[0] as Ball).velocity == -(things[1] as Ball).velocity) {
+        for(Thing thing in things) {
+          if(thing is Ball && !thing.generator) 
+            yield Ball(thing.position, -thing.velocity, thing.color, thing.generator);
+        }
+      } else {
+        for(Thing ball in things) yield ball;
+      }
+    }
+  }
+  }
+}
+
--- main.dart
+++ main.dart
@@ -1,14 +1,21 @@
 import 'logic.dart';
 import 'package:test/test.dart';
+import 'dart:io';
 
 // Required for repl.it
 
 // Used for testing library
-void main() {
-  test("Ball.toString", () {
-    expect(Ball(Vec2(0, 0), Vec2.north()).toString(), "Ball(position: Pos2(0, 0), velocity: Vec2(0, -1))");
-  });
 
+void main() async {
+  Gameboard board = Gameboard.parse(File("game.balls").readAsStringSync());
+  while(true) {
+    print("\u001b[2J\u001b[0;0f$board\n");
+    await Future.delayed(Duration(milliseconds: 200));
+    board.tick();
+  }
+}
+
+void myTest() {
   test("Vec2.rotated", () {
     Vec2 vector = Vec2.north();
     expect(vector = vector.rotated(true), Vec2.east());
@@ -30,5 +37,28 @@ void main() {
     ball = ball.getUpdatedBall();
     expect(ball.velocity, Vec2.east());
     expect(ball.position, Vec2.east() + Vec2(0, 1));
+  });
+
+  group("World tests/", () {
+    Gameboard game = Gameboard.parse(File("test_1.balls").readAsStringSync());
+    test("Gameboard.parse and Gameboard.toString", (){
+      print("Start:\n$game");
+      expect(game.toString(), "#####\n#   #\n# v #\n#   #\n#####");
+    });
+    test("Gameboard.tick moves balls", () {
+      game.tick();
+      print("After 1 tick:\n$game");
+      expect(game.toString(), "##########\n##      ##\n##      ##\n##  vv  ##\n##########");
+    });
+    test("Gameboard.toString; special case", () {
+      game.tick();
+      print("After 2 ticks:\n$game");
+      expect(game.toString(), "##########\n##      ##\n##      ##\n##      ##\n####XX####");
+    });
+    test("Gameboard.tick; bounceback", () {
+      game.tick();
+      print("After 3 ticks:\n$game");
+      expect(game.toString(), "##########\n##      ##\n##      ##\n##  AA  ##\n##########");
+    });
   });
 }
